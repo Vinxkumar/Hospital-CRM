767f902393a702a5279812ea48e58cd48176a0de
Vinxkumar <vinxkumar178@gmail.com>
2026-04-20 10:49:41 +0530

    Updated Security to permit all for testing phase

---
+++ src/main/java/com/example/hospitalCrm/configuration/securityConfig.java
@@ -0,0 +1,27 @@
+package com.example.hospitalCrm.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class securityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(AbstractHttpConfigurer :: disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                ).httpBasic(Customizer.withDefaults());
+
+
+        return http.build();
+
+    }
+}
