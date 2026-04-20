a468a50626469db349e65e060af251b8cd5603b8
Vinxkumar <vinxkumar178@gmail.com>
2026-04-20 10:45:03 +0530

    Initialized Hospital CRM backed

---
+++ src/main/java/com/example/hospitalCrm/HospitalCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.hospitalCrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hospitalCrm/HospitalCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospitalCrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
