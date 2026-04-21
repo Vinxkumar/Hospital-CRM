5db7834a3bbc9ab48feff4bbc46478df4569e20d
Vinxkumar <vinxkumar178@gmail.com>
2026-04-21 18:07:34 +0530

    Create request and response dto's for DoctorEntity

--- src/main/java/com/example/hospitalCrm/dtos/DoctorDto/DoctorRequest.java
+++ src/main/java/com/example/hospitalCrm/dtos/DoctorDto/DoctorRequest.java
@@ -1,4 +1,38 @@
 package com.example.hospitalCrm.dtos.DoctorDto;
 
+
+import com.example.hospitalCrm.dtos.DoctorAvailabilityDto.DoctorAvailabilityResponse;
+
+import com.example.hospitalCrm.dtos.UserDto.UserRequest;
+import com.example.hospitalCrm.type.DoctorDepartment;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class DoctorRequest {
+
+    private UserRequest user;
+
+    private String doctorAlternativePhone;
+
+    private String doctorSpecialization;
+
+    private String doctorQualification;
+
+    private String doctorLicenceNo;
+
+    private Boolean doctorAvailable;
+
+    @Enumerated(EnumType.STRING)
+    private DoctorDepartment doctorDepartment;
+
+    private List<DoctorAvailabilityResponse> doctorAvailability;
+
 }
--- src/main/java/com/example/hospitalCrm/dtos/DoctorDto/DoctorResponse.java
+++ src/main/java/com/example/hospitalCrm/dtos/DoctorDto/DoctorResponse.java
@@ -1,4 +1,34 @@
 package com.example.hospitalCrm.dtos.DoctorDto;
 
+import com.example.hospitalCrm.dtos.DoctorAvailabilityDto.DoctorAvailabilityResponse;
+import lombok.*;
+
+import java.util.List;
+
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class DoctorResponse {
+
+    String jwtToken;
+
+    private String doctorName;
+
+    private String doctorPhone;
+
+    private String doctorAlternativePhone;
+
+    private String doctorSpecialization;
+
+    private String doctorQualification;
+
+    private String doctorLicenceNo;
+
+    private Boolean doctorAvailable;
+
+//    private List<DoctorAvailabilityResponse> doctorAvailability;
 }
