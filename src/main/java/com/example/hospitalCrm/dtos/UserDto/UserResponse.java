8061208db64888593f09a0a921b361ea86a13b28
Vinxkumar <vinxkumar178@gmail.com>
2026-04-21 18:36:22 +0530

    Added Getter and Setter and validators in UserResponse.java dto

--- src/main/java/com/example/hospitalCrm/dtos/UserDto/UserResponse.java
+++ src/main/java/com/example/hospitalCrm/dtos/UserDto/UserResponse.java
@@ -1,8 +1,19 @@
 package com.example.hospitalCrm.dtos.UserDto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserResponse {
+
+    @NotNull
     private String jwtToken;
     private LocalDateTime responseAt;
+
 }
