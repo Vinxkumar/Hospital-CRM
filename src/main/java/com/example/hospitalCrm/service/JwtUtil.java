b1a77599790bdc1b6fd5b37858eee011d1ab9863
Vinxkumar <vinxkumar178@gmail.com>
2026-04-21 18:08:55 +0530

    updated to take UserEntity to generate token

--- src/main/java/com/example/hospitalCrm/service/JwtUtil.java
+++ src/main/java/com/example/hospitalCrm/service/JwtUtil.java
@@ -1,6 +1,7 @@
 package com.example.hospitalCrm.service;
 
 
+import com.example.hospitalCrm.entity.UsersEntity;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
@@ -9,13 +10,10 @@ import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
-import java.security.Key;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @Component
-public class JwtTokenBuilder {
+public class JwtUtil {
 
     @Value("${jwt.secretKey}")
     private String secretKey;
@@ -24,10 +22,10 @@ public class JwtTokenBuilder {
         return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
     }
 
-    public String generateToken(String userName) {
+    public String generateToken(UsersEntity user) {
         return Jwts.builder()
-                .subject(userName)
-                .claim("userName", userName)
+                .subject(user.getUserEmail())
+                .claim("role", user.getRole())
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() +1000*60*5))
                 .signWith(getSecretKey())
