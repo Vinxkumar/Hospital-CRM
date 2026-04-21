64064f039244309a26f664e0ccd729d2a4a9178f
Vinxkumar <vinxkumar178@gmail.com>
2026-04-21 18:32:22 +0530

    Updated UserResponse.java

--- src/main/java/com/example/hospitalCrm/respository/UserRepository.java
+++ src/main/java/com/example/hospitalCrm/respository/UserRepository.java
@@ -2,11 +2,27 @@ package com.example.hospitalCrm.respository;
 
 import com.example.hospitalCrm.entity.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
+@Repository
 public interface UserRepository extends JpaRepository<UsersEntity, Long> {
 
     UsersEntity findByUserName(String userName);
-    UsersEntity findbyUserEmail(String userEmail);
+    UsersEntity findByUserEmail(String userEmail);
     UsersEntity findByUserPhone(String userPhone);
+
+    List<UsersEntity> findByRole(String role);
+
+    boolean existsByUserName(String userName);
+    boolean existsByUserEmail(String userEmail);
+    boolean existsByUserPhone(String userPhone);
+    void deleteByUserName(String UserName);
+    boolean deleteByUserEmail(String userEmail);
+    boolean deleteByUserPhone(String userPhone);
+
 
 }
