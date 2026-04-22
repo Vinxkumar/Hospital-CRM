cf0d66c767ee64731999507b90e11581a4f8907f
Vinxkumar <vinxkumar178@gmail.com>
2026-04-22 18:28:31 +0530

    Updated PatientRequest and PatientResponse Dto's

--- src/main/java/com/example/hospitalCrm/dtos/PatientDto/PatientRequest.java
+++ src/main/java/com/example/hospitalCrm/dtos/PatientDto/PatientRequest.java
@@ -1,4 +1,27 @@
 package com.example.hospitalCrm.dtos.PatientDto;
 
+
+import com.example.hospitalCrm.dtos.UserDto.UserRequest;
+import com.example.hospitalCrm.type.PatientBloodGroup;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PatientRequest {
+
+    private UserRequest user;
+
+    private String emergencyPhone;
+
+    private String patientAddress;
+
+    private PatientBloodGroup patientBloodGroup;
+
+
 }
--- src/main/java/com/example/hospitalCrm/dtos/PatientDto/PatientResponse.java
+++ src/main/java/com/example/hospitalCrm/dtos/PatientDto/PatientResponse.java
@@ -1,4 +1,38 @@
 package com.example.hospitalCrm.dtos.PatientDto;
 
+
+import com.example.hospitalCrm.dtos.AppointmentDto.AppointmentResponse;
+import com.example.hospitalCrm.dtos.PatientCertificate.PatientCertificateResponse;
+import com.example.hospitalCrm.dtos.PrescriptionDto.PrescriptionResponse;
+import com.example.hospitalCrm.type.PatientBloodGroup;
+import jakarta.annotation.Nullable;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class PatientResponse {
+
+    private String jwtToken;
+
+    private String patientName;
+
+    private String patientEmail;
+
+    private String patientPhone;
+
+    private String patientEmergencyPhone;
+
+    private PatientBloodGroup patientBloodGroup;
+
+    private String patientAddress;
+
+    @Nullable
+    private List<AppointmentResponse> patientAppointments;
+    private List<PrescriptionResponse> patientPrescriptions;
+    private List<PatientCertificateResponse> patientCertificates;
 }
