f849334cc2879b2c766bd7c79d3e1fd8a16e4128
Vinxkumar <vinxkumar178@gmail.com>
2026-04-22 18:26:06 +0530

    Updated Doctor Repository

--- src/main/java/com/example/hospitalCrm/respository/DoctorRepository.java
+++ src/main/java/com/example/hospitalCrm/respository/DoctorRepository.java
@@ -4,4 +4,15 @@ import com.example.hospitalCrm.entity.DoctorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DoctorRepository extends JpaRepository<DoctorEntity, Long> {
+
+//    DoctorEntity findByDoctorId(String userEmail);
+
+    DoctorEntity findByDoctorLicenceNo(String doctorLicenceNo);
+
+    Boolean existsByDoctorId(Long id);
+
+    Boolean existsByDoctorLicenceNo(String doctorLicenceNo);
+
+    void deleteByDoctorLicenceNo(String doctorLicenceNo);
+    void deleteByDoctorId(Long id);
 }
