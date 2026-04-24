203b12f9ed3337960dd005d15c48b524b7547c0b
Vinxkumar <vinxkumar178@gmail.com>
2026-04-24 14:34:36 +0530

    Updated Appointment Dto's

--- src/main/java/com/example/hospitalCrm/dtos/AppointmentDto/AppointmentRequest.java
+++ src/main/java/com/example/hospitalCrm/dtos/AppointmentDto/AppointmentRequest.java
@@ -1,4 +1,27 @@
 package com.example.hospitalCrm.dtos.AppointmentDto;
 
+import com.example.hospitalCrm.type.AppointmentStatus;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppointmentRequest {
+
+    private String message;
+
+    private Long doctorId;
+
+    private LocalDate appointmentDate;
+
+    private LocalTime startTime;
+
+    private LocalTime endTime;
+
 }
--- src/main/java/com/example/hospitalCrm/dtos/AppointmentDto/AppointmentResponse.java
+++ src/main/java/com/example/hospitalCrm/dtos/AppointmentDto/AppointmentResponse.java
@@ -1,4 +1,44 @@
 package com.example.hospitalCrm.dtos.AppointmentDto;
 
+
+import com.example.hospitalCrm.type.AppointmentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppointmentResponse {
+
+
+    private String patientName;
+
+    private String message;
+
+    private String doctorName;
+//
+    private String doctorQualification;
+
+    private String doctorSpecialization;
+
+    private Boolean doctorAvailable;
+
+
+    private LocalDate appointmentDate;
+
+    private LocalTime startTime;
+
+    private LocalTime endTime;
+
+    private AppointmentStatus appointmentStatus;
+
+
 }
