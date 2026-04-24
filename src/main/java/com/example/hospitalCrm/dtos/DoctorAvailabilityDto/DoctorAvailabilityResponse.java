7baadedd8c021d81c770086a33dda680a766fc42
Vinxkumar <vinxkumar178@gmail.com>
2026-04-24 14:37:55 +0530

    Removed DoctorAvailabilityEntity and its related Files like servies and repositories

--- src/main/java/com/example/hospitalCrm/dtos/DoctorAvailabilityDto/DoctorAvailabilityResponse.java
+++
@@ -1,4 +0,0 @@
-package com.example.hospitalCrm.dtos.DoctorAvailabilityDto;
-
-public class DoctorAvailabilityResponse {
-}
--- src/main/java/com/example/hospitalCrm/respository/DoctorAvailabilityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hospitalCrm.respository;
-
-import com.example.hospitalCrm.entity.DoctorAvailabilityEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorAvailabilityRepository extends JpaRepository<DoctorAvailabilityEntity, Long> {
-}
