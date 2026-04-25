be55651a472f017a919db3e5a77c6eb7b77ff8df
Vinxkumar <vinxkumar178@gmail.com>
2026-04-25 19:46:20 +0530

    Added New Query Methods

--- src/main/java/com/example/hospitalCrm/respository/PrescriptionRepository.java
+++ src/main/java/com/example/hospitalCrm/respository/PrescriptionRepository.java
@@ -4,4 +4,8 @@ import com.example.hospitalCrm.entity.PrescriptionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PrescriptionRepository extends JpaRepository<PrescriptionEntity, Long> {
+
+    Boolean existsByPatientPatientIdAndDoctorDoctorIdAndAppointmentAppointmentId(Long patientId, Long docId, Long appointmentId);
+    PrescriptionEntity findByPatientPatientIdAndDoctorDoctorIdAndAppointmentAppointmentId(Long patientId, Long docId, Long appointmentId);
+
 }
