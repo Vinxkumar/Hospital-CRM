adbdf574b28f37cbd9195c7024ddaeaf35d1cabd
Vinxkumar <vinxkumar178@gmail.com>
2026-04-25 19:47:48 +0530

    Added PrescriptionItems servicesImplementation

---
+++ src/main/java/com/example/hospitalCrm/service/PrescriptionService.java
@@ -0,0 +1,18 @@
+package com.example.hospitalCrm.service;
+
+import com.example.hospitalCrm.dtos.PrescriptionDto.PrescriptionRequest;
+import com.example.hospitalCrm.dtos.PrescriptionDto.PrescriptionResponse;
+
+import java.util.List;
+
+public interface PrescriptionService {
+
+    PrescriptionResponse createPrescription(Long patientId, Long doctorId, Long appointmentId, PrescriptionRequest request);
+
+    List<PrescriptionResponse> fetchAllByPatientId(Long PatientId);
+
+    List<PrescriptionResponse> fetchAllByDoctorId(Long docId);
+
+    PrescriptionResponse fetchByAppointmentId(Long appointmentId);
+
+}
