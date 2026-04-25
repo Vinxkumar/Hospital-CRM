5d5ec7e90d74ade2e5f84a5229fb44edb445a7f1
Vinxkumar <vinxkumar178@gmail.com>
2026-04-25 19:45:35 +0530

    Added Query Function to fetch PrescriptionItmes by prescriptionId

--- src/main/java/com/example/hospitalCrm/respository/PrescriptionItemsRepository.java
+++ src/main/java/com/example/hospitalCrm/respository/PrescriptionItemsRepository.java
@@ -1,7 +1,13 @@
 package com.example.hospitalCrm.respository;
 
 import com.example.hospitalCrm.entity.PrescriptionEntity;
+import com.example.hospitalCrm.entity.PrescriptionItemsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PrescriptionItemsRepository extends JpaRepository<PrescriptionEntity, Long> {
+
+    List<PrescriptionItemsEntity> findByPrescriptionId(Long  prescriptionId);
+
 }
