69713d3ac788806722126dac36ea9e5cdac33073
Vinxkumar <vinxkumar178@gmail.com>
2026-04-25 19:46:52 +0530

    Added PrescriptionEntity Dto's

--- src/main/java/com/example/hospitalCrm/dtos/PrescriptionDto/PrescriptionRequest.java
+++ src/main/java/com/example/hospitalCrm/dtos/PrescriptionDto/PrescriptionRequest.java
@@ -1,4 +1,27 @@
 package com.example.hospitalCrm.dtos.PrescriptionDto;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PrescriptionRequest {
+
+    private Long appointmentId;
+
+    private Long doctorId;
+
+    private Long patientId;
+
+    private String patientDiagnosis;
+
+    private LocalDateTime issuedAt;
+
+
+
 }
--- src/main/java/com/example/hospitalCrm/dtos/PrescriptionDto/PrescriptionResponse.java
+++ src/main/java/com/example/hospitalCrm/dtos/PrescriptionDto/PrescriptionResponse.java
@@ -1,4 +1,37 @@
 package com.example.hospitalCrm.dtos.PrescriptionDto;
 
+import com.example.hospitalCrm.dtos.PrescriptionItemsDto.PrescriptionItemsResponse;
+import com.example.hospitalCrm.type.DoctorDepartment;
+import lombok.*;
+
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PrescriptionResponse {
+
+    private LocalDate appointmentDate;
+
+    private String doctorName;
+
+    private String doctorSpecialization;
+
+    private DoctorDepartment doctorDepartment;
+
+    private LocalTime startTime;
+
+    private LocalTime endTime;
+
+    private String patientDiagnosis;
+
+    private List<PrescriptionItemsResponse> prescriptionItems;
+
 }
