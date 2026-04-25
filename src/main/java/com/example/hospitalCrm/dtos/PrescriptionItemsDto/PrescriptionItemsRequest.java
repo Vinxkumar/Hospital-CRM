b60d78da1cbbbe60e30f0ff9b0ad4fbf81e009d3
Vinxkumar <vinxkumar178@gmail.com>
2026-04-25 19:46:01 +0530

    Added PrescriptionItems Dto's

--- src/main/java/com/example/hospitalCrm/dtos/PrescriptionItemsDto/PrescriptionItemsRequest.java
+++ src/main/java/com/example/hospitalCrm/dtos/PrescriptionItemsDto/PrescriptionItemsRequest.java
@@ -1,4 +1,25 @@
 package com.example.hospitalCrm.dtos.PrescriptionItemsDto;
 
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PrescriptionItemsRequest {
+
+    private Long prescriptionId;
+
+    private Long medicineId;
+
+    private String medicineDosage;
+
+    private String prescriptionFrequency;
+
+    private Long prescriptionDurationInDays;
+
+    private String doctorNote;
+
 }
--- src/main/java/com/example/hospitalCrm/dtos/PrescriptionItemsDto/PrescriptionItemsResponse.java
+++ src/main/java/com/example/hospitalCrm/dtos/PrescriptionItemsDto/PrescriptionItemsResponse.java
@@ -1,4 +1,15 @@
 package com.example.hospitalCrm.dtos.PrescriptionItemsDto;
 
+
 public class PrescriptionItemsResponse {
+
+    private String medicineName;
+
+    private String medicineDosage;
+
+    private String prescriptionFrequency;
+
+    private Long prescriptionDurationInDays;
+
+    private String doctorNote;
 }
