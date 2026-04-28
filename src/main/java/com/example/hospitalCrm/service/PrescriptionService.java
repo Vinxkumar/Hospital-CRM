6dbae08207319368241c173875922965385e28e3
Vinxkumar <vinxkumar178@gmail.com>
2026-04-28 23:20:39 +0530

    Added New Query Function Method

--- src/main/java/com/example/hospitalCrm/service/PrescriptionService.java
+++ src/main/java/com/example/hospitalCrm/service/PrescriptionService.java
@@ -2,12 +2,13 @@ package com.example.hospitalCrm.service;
 
 import com.example.hospitalCrm.dtos.PrescriptionDto.PrescriptionRequest;
 import com.example.hospitalCrm.dtos.PrescriptionDto.PrescriptionResponse;
+import com.example.hospitalCrm.dtos.PrescriptionItemsDto.PrescriptionItemsRequest;
 
 import java.util.List;
 
 public interface PrescriptionService {
 
-    PrescriptionResponse createPrescription(Long patientId, Long doctorId, Long appointmentId, PrescriptionRequest request);
+    PrescriptionResponse createPrescription(Long patientId, Long doctorId, Long appointmentId, PrescriptionRequest request, List<PrescriptionItemsRequest> prescriptionItemsRequests);
 
     List<PrescriptionResponse> fetchAllByPatientId(Long PatientId);
 
