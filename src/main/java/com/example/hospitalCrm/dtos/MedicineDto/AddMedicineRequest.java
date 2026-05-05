4f4bb2204dcd9ccdb73302a70a83516e0f026b62
Vinxkumar <vinxkumar178@gmail.com>
2026-05-05 21:25:38 +0530

    Added Request DTO's for MedicineEntity

---
+++ src/main/java/com/example/hospitalCrm/dtos/MedicineDto/AddMedicineRequest.java
@@ -0,0 +1,32 @@
+package com.example.hospitalCrm.dtos.MedicineDto;
+
+
+import com.example.hospitalCrm.dtos.MedicineInventoryDto.InventoryRequest;
+import com.example.hospitalCrm.type.MedicineCategory;
+import com.example.hospitalCrm.type.MedicineDosageForm;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddMedicineRequest {
+
+    private String medicineFullName;
+
+    private String medicineGenericName;
+
+    private MedicineDosageForm dosageForm;
+
+    private List<MedicineCategory> medicineCategories;
+
+    private String manufacturer;
+
+    private String strengthMg_Ml;
+
+    private InventoryRequest inventoryRequest;
+
+}
