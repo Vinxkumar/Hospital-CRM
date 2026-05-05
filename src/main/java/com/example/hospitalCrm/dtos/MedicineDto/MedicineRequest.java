f4b304f50eb8be81427d066ea4fb9e7883c46042
Vinxkumar <vinxkumar178@gmail.com>
2026-05-05 21:21:09 +0530

    Added Request and Response DTO's for MedicineEntiy

--- src/main/java/com/example/hospitalCrm/dtos/MedicineDto/MedicineRequest.java
+++ src/main/java/com/example/hospitalCrm/dtos/MedicineDto/MedicineRequest.java
@@ -1,4 +1,29 @@
 package com.example.hospitalCrm.dtos.MedicineDto;
 
+
+import com.example.hospitalCrm.type.MedicineCategory;
+import com.example.hospitalCrm.type.MedicineDosageForm;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MedicineRequest {
+
+    private String medicineFullName;
+
+    private String medicineGenericName;
+
+    private MedicineDosageForm dosageForm;
+
+    private List<MedicineCategory> medicineCategories;
+
+    private String manufacturer;
+
+    private String strengthMg_Ml;
+
 }
--- src/main/java/com/example/hospitalCrm/dtos/MedicineDto/MedicineResponse.java
+++ src/main/java/com/example/hospitalCrm/dtos/MedicineDto/MedicineResponse.java
@@ -1,4 +1,25 @@
 package com.example.hospitalCrm.dtos.MedicineDto;
 
+
+import com.example.hospitalCrm.dtos.MedicineInventoryDto.InventoryResponse;
+import com.example.hospitalCrm.type.MedicineCategory;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MedicineResponse {
+
+    private String medicineFullName;
+
+    private List<MedicineCategory> medicineCategories;
+
+    private String strengthMg_Ml;
+
+    private InventoryResponse inventoryResponse;
+
 }
