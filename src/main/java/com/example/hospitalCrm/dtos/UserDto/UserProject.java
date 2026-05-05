2ec7a6b91c5edf983234a42eefd575c984bdf2de
Vinxkumar <vinxkumar178@gmail.com>
2026-05-05 21:15:16 +0530

    Updated UserProject.java Dto to return the role of the user as well for fe

--- src/main/java/com/example/hospitalCrm/dtos/UserDto/UserProject.java
+++ src/main/java/com/example/hospitalCrm/dtos/UserDto/UserProject.java
@@ -1,9 +1,12 @@
 package com.example.hospitalCrm.dtos.UserDto;
 
+import com.example.hospitalCrm.type.Role;
+
 public interface UserProject {
 
         String getUserName();
         String getUserEmail();
         String getUserPhone();
+        Role getRole();
 
 }
